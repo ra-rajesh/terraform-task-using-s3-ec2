efc119668b69f02cec117bc1400f60c6bce9cb63
Rajesh <rajesh@xxx.com>
2024-12-04 23:49:04 +0530

    access key add

--- main.tf
+++ main.tf
@@ -9,7 +9,9 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = "" # AWS account rajesh_rajendiran
+  secret_key = "" # AWS account rajesh_rajendiran
 }
 
 # Configure the AWS S3 under rajesh_rajendiran
